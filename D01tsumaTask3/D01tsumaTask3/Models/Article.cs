ee1f6614b8eb98c3691c7a984ffe197c95536883
d01tsuma <d01tsumath@sigmact.com>
2019-08-30 16:21:30 +0900

    Titleクラス->Articleクラスへ変更

---
+++ D01tsumaTask3/D01tsumaTask3/Models/Article.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace D01tsumaTask3.Models
+{
+    public class Article
+    {
+        public string Title { get; }
+        public string Url { get; }
+
+        public Article(string title, string url)
+        {
+            this.Title = title;
+            this.Url = url;
+        }
+    }
+
+    /*public class Title
+    {
+        public string Name { get; set; }
+        public string URL { get; set; }
+    }*/
+}
